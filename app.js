b9e0b941ecc481fc6f8cc13996e163901d9ec05b
ABartolomeu-coder <124580703+ABartolomeu-coder@users.noreply.github.com>
2023-10-23 18:03:59 +0100

    Generate first color; Checking contrast text; Update CSS

---
+++ app.js
@@ -0,0 +1,46 @@
+//Global selections and variables
+const colorDivs = document.querySelectorAll(".color");
+const generateBtn = document.querySelector(".generate");
+const sliders = document.querySelectorAll('input[type="range"]');
+const currentHexes = document.querySelectorAll(".color h2");
+let initialColors;
+//Functions
+
+//Color Generator
+function generateHex() {
+  //Manual
+  //const letters = "#0123456789ABCDEF";
+  //let hash = "#";
+  //for (let i = 0; i < 6; i++) {
+  //  hash += letters[Math.floor(Math.random() * 16)];
+  //}
+  //return hash;
+
+  //Using Chroma JS
+  const hexColor = chroma.random();
+  return hexColor;
+}
+
+function randomColor() {
+  colorDivs.forEach((div, index) => {
+    const hexText = div.children[0];
+    const randomColor = generateHex();
+
+    //Add the color to the background
+    div.style.backgroundColor = randomColor;
+    hexText.innerText = randomColor;
+    //check for contrast
+    checkTextContrast(randomColor, hexText);
+  });
+}
+
+function checkTextContrast(color, text) {
+  const luminance = chroma(color).luminance();
+  if (luminance > 0.5) {
+    text.style.color = "black";
+  } else {
+    text.style.color = "white";
+  }
+}
+
+randomColor();
